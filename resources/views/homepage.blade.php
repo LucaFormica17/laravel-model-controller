82d59f509a18eebeca069f36c8f28a9fe4d1130b
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2024-02-15 09:53:16 +0100

    [UPDATE] change port_database and view on screen

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -17,7 +17,23 @@
 
 <body>
 
-    
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    @foreach ($movies as $movie)    
+                    <div class="card text-center py-3 my-3">
+                        <h2>{{$movie['title']}}</h2>
+                        <h4>{{$movie['original_title']}}</h4>
+                        <span>Paese: {{$movie['nationality']}}</span>
+                        <span>Data Uscita: {{$movie['date']}}</span>
+                        <span>Voto: {{$movie['vote']}}</span>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </main>
 
 </body>
 
